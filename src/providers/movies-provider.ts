0cde24de20a58990a5136fec2cca50be6d238cf5
Diego Mena <Diego Mena>
2017-02-12 18:52:40 -0400

    Uploads images to server

--- src/providers/movies-provider.ts
+++ src/providers/movies-provider.ts
@@ -2,39 +2,32 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the MoviesProvider provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class MoviesProvider {
 
   private data;
 
+  public progressBarValue: any = 0;
+
+  public progressBarClass: any = "hide";
+  
+
   constructor(public http: Http) {
     console.log('Hello MoviesProvider Provider');
+    document.addEventListener("deviceready", onDeviceReady, false);
+    function onDeviceReady() {
+    }
   }
 
   load() {
     if (this.data) {
-      // already loaded data
       return Promise.resolve(this.data);
     }
-
-    // don't have the data yet
     return new Promise(resolve => {
-      // We're using Angular HTTP provider to request the data,
-      // then on the response, it'll map the JSON data to a parsed JS object.
-      // Next, we process the data and resolve the promise with the new data.
       this.http.get('http://45.55.77.201:8084/movies/json')
         .map(res => res.json())
         .subscribe(data => {
-          // we've got back the raw data, now generate the core schedule data
-          // and save the data for later reference
           this.data = data;
-          console.log(data);
           resolve(this.data);
         });
     });
